3c56a0764cd453a9a26ddded2baae2784f918b9b
Peter Cheune <peterkayk@gmail.com>
2022-11-21 11:17:45 +0300

    second commit

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,7 +1,7 @@
+// ignore_for_file: prefer_const_constructors, duplicate_ignore
+
 import 'package:flutter/material.dart';
 import 'package:flutter_todo_app/widgets.dart';
-
-
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -18,11 +18,11 @@ class _HomepageState extends State<Homepage> {
       body: SafeArea(
         child: Container(
           width: double.infinity,
-          padding: const EdgeInsets.symmetric(
+          padding: EdgeInsets.symmetric(
             horizontal: 24.0,
             vertical: 32.0,
           ),
-          color: const Color(0XFFF6F6F6),
+          color: Color(0XFFF6F6F6),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             // ignore: prefer_const_literals_to_create_immutables
@@ -32,17 +32,18 @@ class _HomepageState extends State<Homepage> {
                 margin: EdgeInsets.only(
                   bottom: 32.0,
                 ),
-                child: const Image(
-                  image: AssetImage(
-                    'assets/images/todo_logo.png'
-                  )
-                ),
+                child: Image(image: AssetImage('assets/images/todo_logo.png')),
               ),
-            const TaskCardWidget(),
-          ],
+              // ignore: prefer_const_constructors
+              TaskCardWidget(
+                title: "Get started",
+                desc: "Hello User! Welcome to WHAT_TO_DO app, this is a default Task that you can edit or delete",
+              ),
+              TaskCardWidget(),
+            ],
+          ),
         ),
       ),
-     ),
     );
   }
 }
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,14 +1,18 @@
+// ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class TaskCardWidget extends StatelessWidget {
-  const TaskCardWidget({super.key});
+  final String title;
+  final String desc;
+  TaskCardWidget({super.key, this.title, this.desc});
 
   @override
   Widget build(BuildContext context) {
     // ignore: avoid_unnecessary_containers
     return Container(
       width: double.infinity,
-      padding: const EdgeInsets.symmetric(
+      padding: EdgeInsets.symmetric(
         horizontal: 24.0,
         vertical: 32.0,
       ),
@@ -18,9 +22,9 @@ class TaskCardWidget extends StatelessWidget {
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
+        children: [
           Text(
-            "Get Started!",
+            title ?? "(Unnamed Task)",
             style: TextStyle(
               color: Color.fromRGBO(33, 21, 81, 1),
               fontSize: 22.0,
@@ -28,7 +32,7 @@ class TaskCardWidget extends StatelessWidget {
             ),
           ),
           Text(
-            "Hello User! Welcome to the TODO app, this is a default task that you can edit or delete to start using the app.",
+            desc ?? "No Description Added",
             style: TextStyle(
               fontSize: 16.0,
               color: Color(0xff86829d),
